9808ea5b38873f70ab41fc63b353b7bfa568bc77
Juan Jose Perdomo Forero <juan.perdomo@ceiba.com.co>
2021-02-09 10:21:01 -0500

    se adiciona funcionalidad de buscar detalle de una reserva por identificador unico de reserva

--- PaquetesTuristicos/infraestructura/src/main/java/com/ceiba/reserva/controlador/ConsultaControlador.java
+++ PaquetesTuristicos/infraestructura/src/main/java/com/ceiba/reserva/controlador/ConsultaControlador.java
@@ -4,6 +4,7 @@ import com.ceiba.reserva.modelo.dto.DtoReserva;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -17,4 +18,10 @@ public interface ConsultaControlador {
             value = "Listar reservas",
             notes = "Lista todas la reservas creadas en el sistema")
     List<DtoReserva> listar();
+
+    @GetMapping("/{identificadorReserva}")
+    @ApiOperation(
+            value = "Detallar reserva",
+            notes = "Detalla una reserva por identificador unico de reserva creada en el sistema")
+    DtoReserva detallarPorIdentificadorReserva(@PathVariable String identificadorReserva);
 }
