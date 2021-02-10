c56ecb5bae604b6bba64c2a5c08ab32090bab34b
Juan Jose Perdomo Forero <juan.perdomo@ceiba.com.co>
2021-02-10 12:11:16 -0500

    se ajustan metodos de utilidad para descuento y cobro

--- PaquetesTuristicos/dominio/src/main/java/com/ceiba/reserva/util/DescuentoCobroUtil.java
+++ PaquetesTuristicos/dominio/src/main/java/com/ceiba/reserva/util/DescuentoCobroUtil.java
@@ -7,14 +7,15 @@ public class DescuentoCobroUtil {
     public static final BigDecimal CINCO_PORCIENTO = new BigDecimal("0.05");
     public static final BigDecimal DIES_PORCIENTO = new BigDecimal("0.1");
     public static final BigDecimal VEINTE_PORCIENTO = new BigDecimal("0.2");
+    private static final BigDecimal PORCENTAJE = new BigDecimal(100);
 
     private DescuentoCobroUtil() {}
 
     public static BigDecimal aplicarDescuento(BigDecimal valor, BigDecimal descuento) {
-        return valor.subtract(valor.divide(descuento, BigDecimal.ROUND_HALF_UP));
+        return valor.subtract((valor.multiply(descuento).divide(PORCENTAJE, BigDecimal.ROUND_HALF_UP)));
     }
 
     public static BigDecimal aplicarCobro(BigDecimal valor, BigDecimal cobro) {
-        return valor.add(valor.divide(cobro, BigDecimal.ROUND_HALF_UP));
+        return valor.add((valor.multiply(cobro).divide(PORCENTAJE, BigDecimal.ROUND_HALF_UP)));
     }
 }
